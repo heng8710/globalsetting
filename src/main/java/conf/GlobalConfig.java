be0cfca695345e7038f21f62d30e775b6a407720
821600682@qq.com <821600682@qq.com>
2015-08-06 11:24:12 +0800

    支持其他的配置js文件。（都必须在classes目录之下）。 默认如果不写引用 的配置js文件的话，是指classes/global.js

--- src/main/java/conf/GlobalConfig.java
+++ src/main/java/conf/GlobalConfig.java
@@ -17,31 +17,90 @@ import com.google.common.io.Files;
  * 全局配置文件的路径 ：classpath:global.js
  * 注意：相对的路径层次必须是这样：
  *   ..../classes/global.js
+ *   ..../classes/xxx/yyy.js
  *   ..../lib/xxx.jar
  */
 public class GlobalConfig {
 
-	private static final String ConfigFileName = "global.js";
-	public static final Path GlobalJsPath;
-	static {
+	static final String DefaultConfigFileName = "global.js";
+	
+//	static Path configPath(){
+//		return configPath(DefaultConfigFileName);
+//	}
+	
+	/**得到【fileName.js】的配置文件的路径。
+	 * 处理了本类处于两种情况下（jar中、classes目录下），的两种情况：在这两种情况下自动找到配置的js文件。
+	 * @param fileName：相对于[classes]的路径；基于文件系统的路径表示方法，最好是用[/]作为分隔符。
+	 * 
+	 * 要小心了：千万不能以[/]或者[\]开头：【/xxx.js】，【\xxx.js】不然就会变成类似于这样：【e:\xxx.js】的样的路径了。
+	 * 
+	 * @return
+	 */
+	static Path configPath(final String fileName/*without [.js] subfix*/){
+		final String _fileName = (!Strings.isNullOrEmpty(fileName))? fileName.trim(): fileName;
+		if(Strings.isNullOrEmpty(_fileName) || _fileName.startsWith("/") || _fileName.startsWith("\\")){
+			throw new IllegalArgumentException(String.format("fileName=[%s]不对", fileName));
+		}
 		try {
+			final Path r;
 			final boolean isWindows = isWindows();
+			//如果是在jar中就是下面的路径：
 			//【jar:file:/F:/ftp/dailijob/lib/globalsetting-1.0.0.jar!/conf/】
 			//【jar:file:/data/application/dailijob/lib/globalsetting-1.0.0.jar!/conf/】
 			final String s = GlobalConfig.class.getResource("").toString();
-			final String jarFilePath = s.substring(isWindows? 10: 9, s.lastIndexOf("!"));
-			//指定配置文件路径
-			GlobalJsPath = Paths.get(jarFilePath).getParent()/*lib*/.getParent().resolve("classes")/*classes*/.resolve(ConfigFileName);
-//			GlobalJsPath = Paths.get(GlobalConfig.class.getResource("/").toURI()).resolve(ConfigFileName).toFile().getAbsolutePath();
+			if(s.toLowerCase().startsWith("jar")){
+				final String jarFilePath = s.substring(isWindows? 10: 9, s.lastIndexOf("!"));
+				//指定配置文件路径
+				r = Paths.get(jarFilePath).getParent()/*lib*/.getParent().resolve("classes")/*classes*/.resolve(_fileName);
+			}else{
+				//【E:\eclipsews\workspace001\globalsetting\target\classes】
+				final Path cp/*classpath:.*/ = Paths.get(GlobalConfig.class.getResource("/").toURI());
+				r = cp.resolve(_fileName);//?多层子路径
+			}
+//			System.out.println("path="+r);
+			return r;
 		} catch (Exception e) {
-			throw new IllegalStateException("cannot find the path of global.js");
+			throw new IllegalStateException(String.format("cannot find the path of [%s.js]", _fileName), e);
 		}
 	}
 	
-	public static void main(String...strings){
+	public static void main(String...strings) throws URISyntaxException{
 		System.out.println(get());
-		System.out.println(get("a"));
-		System.out.println(get("mail.host"));
+		System.out.println(getByPath("a"));
+		System.out.println(getByPath("mail.host"));
+		
+		System.out.println(get("another.js"));
+		System.out.println(getByPath("another.js", "a"));
+		System.out.println(getByPath("another.js", "mail.host"));
+		
+		System.out.println(get("noexit.js"));
+		System.out.println(getByPath("noexit.js", "a"));
+		System.out.println(getByPath("noexit.js", "mail.host"));
+		
+//		System.out.println(get("/another.js"));//不要以[/开头]
+//		System.out.println(getByPath("/another.js", "a"));
+//		System.out.println(getByPath("/another.js", "mail.host"));
+		
+//		System.out.println(get("\\another.js"));//不要以[\\开头]
+//		System.out.println(getByPath("\\another.js", "a"));
+//		System.out.println(getByPath("\\another.js", "mail.host"));
+		
+		
+//		Path p = Paths.get("d:/").resolve("ssha");
+//		System.out.println(p);
+		
+		
+		
+//		Path p2 = Paths.get(GlobalConfig.class.getResource("/").toURI());
+//		System.out.println(p2);
+//		
+//		
+//		Path p3 = Paths.get(GlobalConfig.class.getResource("").toURI());
+//		System.out.println(p3);
+//		
+//		
+//		Path p4 = Paths.get(GlobalConfig.class.getResource(".").toURI());
+//		System.out.println(p4);
 		
 	}
 	
@@ -50,30 +109,49 @@ public class GlobalConfig {
 	 * @param path --按点.分隔，比如：【mail.host】（这也意味阒所有key不能有点号在中间）
 	 * @return
 	 */
-	public static Object get(final String path){
+	public static Object getByPath(final String path){
+		return getByPath(DefaultConfigFileName, path);
+	}
+	
+	/**
+	 * @param configFilePath　：相对于[classes]的路径；基于文件系统的路径表示方法，最好是用[/]作为分隔符，【要带.js结尾（注意大小 写）】
+	 * @param path ：用[.]分隔，类型于对象的属性值引用表示方法。
+	 * @return
+	 */
+	public static Object getByPath(final String configFilePath, final String path){
 		try {
-			final Map<String, Object > root  = get();
+			final Map<String, Object > root  = get(configFilePath);
 			Object tmp = root;
 			for(final String key: Splitter.on('.').omitEmptyStrings().trimResults().split(Strings.nullToEmpty(path))){
 				if(tmp instanceof Map){
 					final Map map = (Map)tmp;
 					tmp = map.get(key);
 				}else{
-					throw new IllegalArgumentException(String.format("path=[%s], 太长了？", path));
+					throw new IllegalArgumentException(String.format("path=[%s], 太长了，或者是路径不对？", path));
 				}
 			}
 			return tmp;
 		} catch (Exception e) {
-			throw new IllegalStateException(String.format("读取配置文件=[%s]失败", ConfigFileName) , e);
+			throw new IllegalStateException(String.format("读取配置文件=[%s]失败", DefaultConfigFileName) , e);
 		}
 	}
 	
+	
+	
 	public static Map<String,Object> get( ){
+		return get(DefaultConfigFileName);
+	}
+	
+	/**
+	 * @param configFilePath：相对于[classes]的路径；基于文件系统的路径表示方法，最好是用[/]作为分隔符，【要带.js结尾（注意大小 写）】
+	 * @return
+	 */
+	public static Map<String,Object> get(final String configFilePath){
 		try {
 			//启动js引擎
 			final ScriptEngineManager sem = new ScriptEngineManager();
 			final ScriptEngine engine = sem.getEngineByName("javascript");
-		    final String objStr = Files.toString(GlobalJsPath.toFile(), Charsets.UTF_8);
+		    final String objStr = Files.toString(configPath(configFilePath).toFile(), Charsets.UTF_8);
 		    //把配置对象字面量（js），转成json格式字符串出来
 		    final String str = (String)engine.eval("JSON.stringify("+objStr+");");
 		    //然后，再转成java中的LinkedHashMap
@@ -81,9 +159,11 @@ public class GlobalConfig {
 //		    logger.debug("global.js : "+ map);
 			return map;
 		} catch (Exception e) {
-			throw new IllegalStateException(String.format("读取配置文件=[%s]失败", ConfigFileName) , e);
+			throw new IllegalStateException(String.format("读取配置文件=[%s]失败", DefaultConfigFileName) , e);
 		}
 	}
+	
+	
 	
 	
 	static boolean isWindows(){
